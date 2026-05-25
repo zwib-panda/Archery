aad02f3f3e713b07201b91847fe6ed3f99fc2caf
zwib-panda <zwib.ho@gmail.com>
2026-05-25 20:35:59 +0200

    Add files via upload

--- archery-sw.js
+++ archery-sw.js
@@ -1,5 +1,5 @@
 // Service Worker — ArcheryLog PWA
-const CACHE="archerylog-v6";
+const CACHE="archerylog-v8";
 const ASSETS=["./archery-pwa.html","./archery-manifest.json","./archery-sw.js"];
 
 self.addEventListener("install",e=>{
